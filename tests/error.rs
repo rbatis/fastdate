5667b71f32c96cba43008681317bb9c98c69f8d0
zxj <zhuxiujia@qq.com>
2023-11-15 17:19:54 +0800

    add test

---
+++ tests/error.rs
@@ -0,0 +1,26 @@
+use std::fmt::{Debug, Display, Formatter};
+use fastdate::error::Error;
+
+#[test]
+fn test_from_str() {
+    let e= Error::from("e");
+    assert_eq!(e.to_string(),"e");
+}
+
+#[test]
+fn test_from_string() {
+    let e= Error::from("e".to_string());
+    assert_eq!(e.to_string(),"e");
+}
+
+#[test]
+fn test_display() {
+    let e= Error::from("e".to_string());
+    assert_eq!(format!("{}",e),"e");
+}
+
+#[test]
+fn test_default() {
+    let e= Error::default();
+    assert_eq!(format!("{}",e),"");
+}
